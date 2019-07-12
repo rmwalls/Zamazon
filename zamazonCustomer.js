178b57fb122ca65746b736230d93b8eaaae034dd
ZelleM <groovyrlm@gmail.com>
2019-07-12 15:43:26 -0400

    security fix lodash

--- zamazonCustomer.js
+++ zamazonCustomer.js
@@ -1,6 +1,8 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+const chalk = require('chalk');
+
 var itemsListArray;
 
 //================= END of REQUIRE Section ===================================
@@ -23,16 +25,16 @@ connection.connect(function(err){
 
 //========================== END of DATABASE INITIALIZATION ===========================
 
-// Which functionality is requested
+// Which functionality is requested - may later add supervisor & manager
 function start() {
 	console.log("\nWELCOME TO ZAMAZON, Your Amazon Alternative!")
     inquirer.prompt({
         name: "which",
         type: "list",
-        message: "Please select your program need (*some require id/pw)",
-        choices: ["Customer", "Manager*", "Supervisor*", "Exit"]
+        message: "Would you like to place an Order or Exit?",
+        choices: ["Order", "Exit"]
     }).then(function(answer){
-        if (answer.which === "Customer") {
+        if (answer.which === "Order") {
             showProducts(); 
         } else {
             endProgram();
@@ -40,16 +42,16 @@ function start() {
     });
 } // end start
 
-// end the program (if Exit selected)
+// end the program if Exit selected
 function endProgram() {
 	inquirer.prompt({
 		name: "end",
 		type: "list",
 		message: "Are you sure you want to exit?",
 		choices: ["Yes", "No"]
-}).then(function(answer){
+	}).then(function(answer){
 		if (answer.end === "Yes") {
-		console.log("choice = " + answer.end);
+		//console.log("choice = " + answer.end);
 		console.log("Thank you for visiting Zamazon!");
 		connection.end()
 	}else {
@@ -59,78 +61,59 @@ function endProgram() {
 } //end endProgram
 
 
-// function to show products to customers
+// function to show products to customers ++++ products displayed
 function showProducts() {
     connection.query("SELECT item_id, product_name, price, stock_quantity FROM products WHERE stock_quantity > 0", function(err, result) {
         if (err) throw console.log("Error: " + err);
         itemsListArray = []
         for(var i = 0; i < result.length; i++){
 			itemsListArray.push(result[i])
-			console.table("Available Items", itemsListArray);
 		} //end for
-    chooseProduct(itemsListArray); //pass products to next function
+		console.table("\nAvailable Items", itemsListArray);
+    	chooseProduct(); 
     }); //end query
 } // end showProducts 
 
 // select product and quantity
 function chooseProduct(){
- 	inquirer.prompt([
-	{
- 		name: "toBuy",
- 		message:"Enter ITEM_ID of item you wish to purchase:",
-     	type: "number"
-	 } //end inq
-	]
-	).then(function(answer) {
-		let item = parseInt(answer.toBuy);
-		console.log("item is " + item);
-		let query = `SELECT * FROM products WHERE item_id = ?`;
-		connection.query(query, item_id, function(err, res){
-			if (err) throw err ;{
-				if(isNaN(item)) {
-					console.log("Invalid item, please try again");
-					chooseProduct();
-				} else {
-					howMany();
-				} //end else
-			} // end if
-		}); //end connectionQuery
-	}) //end then
-} //end chooseProduct
-
-function howMany(){
 	inquirer.prompt([
+   		{
+			name: "toBuy",
+			type: "number",
+			message:"Enter ITEM_ID of item you wish to purchase:"
+		}, 
 		{
 			name: "amount",
 			type: "number",
 			message: "How many do you want?"
 		} //end inq
-	]
-	).then(function(answer) {
-		let quantity = parseInt(answer.amount);
-		console.log("quant is " + quantity);
-		connection.query(query, stock_quantity, function(err, res){
-			if (err) throw err ; {
-				if (quantity > parseInt(res.stock_quantity)) {
-				} else {
-					console.log("Your request exceeds the amount available, please try again");
-					howMany();
-				}
-			} //end if
-		});
-	})
+   ]
+   ).then(function(answer) {
+	   let itemID = parseInt(answer.toBuy);
+	   //console.log("item is " + itemID);
+	   let quantity = parseInt(answer.amount);
+	   //console.log("quant is " + quantity);
+	   buyProduct(itemID, quantity);
+   })
 }
 
-//get item they want to buy then askuantity
-	//check that entry is valid(parseInt the id number)
-		// if not valid, sorry that is not an item, please try again, redisplay chooseProduct
-	// check that quantity is available
-		// if zero, sorry out of stock
-		// if not enough, sorry there are only ___ available
-		//Do you want to buy those?
-			// yes, continue to buyProduct
-			//no, redisplay
-
-//go here if product available and customer continues
-// function buyProduct(){
-//} end buyProduct
+function buyProduct(id, quantity) {
+	//console.log("id is " + id + " quantity is " + quantity);
+    connection.query("SELECT * FROM products WHERE item_id = " + id, function (err, res) {
+        if (err) { console.log(err); }
+        //console.log(res[0].price);
+			if (quantity <= res[0].stock_quantity) {
+				let totalCost = parseFloat(res[0].price * quantity).toFixed(2);
+				console.log(chalk.green("\nYour total cost for " + quantity + " " + res[0].product_name + " is $" + totalCost + "."));
+				console.log("\n")
+				connection.query("UPDATE products SET stock_quantity = stock_quantity - " + quantity + " WHERE item_id = " + id);
+				console.log(chalk.magenta("Thank you for shopping at Zamazon! Come back again!\n"));
+				connection.end()
+			} else {
+				console.log("\n")
+				console.log(chalk.bgRed.bold("We do not have enough in stock. Please try again."))
+				console.log("\n")
+				showProducts();
+			}
+    });
+};
